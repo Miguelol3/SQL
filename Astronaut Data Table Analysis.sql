d11b68463d84169e9d8357ef8665238aa9cfa53f
Miguel Ojeda <126533982+Miguelol3@users.noreply.github.com>
2023-03-14 19:26:00 -0400

    Update Astronaut Data Table Analysis.sql

--- Astronaut Data Table Analysis.sql
+++ Astronaut Data Table Analysis.sql
@@ -9,10 +9,10 @@ GROUP BY gender;
 -- In terms of education and military rank, what does it 'take' to be an Astronaut?
 SELECT 
 CASE
-    WHEN military_rank is NOT NULL AND (undergraduate_major IS NOT NULL OR graduate_major IS NOT NULL) then "Ranked Officer, Degree Obtained"
-    WHEN military_rank is NOT NULL AND (undergraduate_major IS NULL AND graduate_major IS NULL) then "Ranked Officer, No Degree"
-    WHEN military_rank is NULL AND (undergraduate_major IS NOT NULL OR graduate_major IS NOT NULL) then "Not an Officer, Degree Obtained"
-    ELSE "Not an Officer, No Degree"
+    WHEN military_rank IS NOT NULL AND (undergraduate_major IS NOT NULL OR graduate_major IS NOT NULL) then 'Ranked Officer, Degree Obtained'
+    WHEN military_rank IS NOT NULL AND (undergraduate_major IS NULL AND graduate_major IS NULL) then 'Ranked Officer, No Degree'
+    WHEN military_rank IS NULL AND (undergraduate_major IS NOT NULL OR graduate_major IS NOT NULL) then 'Not an Officer, Degree Obtained'
+    ELSE 'Not an Officer, No Degree'
 END AS rank_and_education,
 COUNT(*)
 FROM astronauts
@@ -20,13 +20,13 @@ GROUP BY rank_and_education
 ORDER BY 2 desc;
 
 -- I want to know which Astronauts were part of the series of Apollo missions...
-SELECT name FROM astronauts
+SELECT name, missions FROM astronauts
 WHERE missions LIKE '%Apollo%';
 
 -- Which institutions can claim to be the Alma Mater for at least 4 Astronauts?
 SELECT alma_mater, COUNT(*) FROM astronauts
 GROUP BY alma_mater
-HAVING count(*) >= 4
+HAVING COUNT(*) >= 4
 ORDER BY 2 DESC
 
 -- What was the average number of hours spent doing spacewalks for those Astronauts who managed to do it at least once?
