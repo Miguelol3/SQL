6a20819bfe1ad60973e1d9c8b6a8671ce0dc7463
Miguel Ojeda <126533982+Miguelol3@users.noreply.github.com>
2023-03-15 21:16:12 -0400

    Update Astronaut Data.sql

--- Astronaut Data.sql
+++ Astronaut Data.sql
@@ -1,12 +1,12 @@
-/* I perfomed the following analysis on the 'NASA Astronauts, 1959-Present' dataset from Kaggle.
-Link to Dataset in Kaggle: https://www.kaggle.com/nasa/astronaut-yearbook
-Table source: https://gist.github.com/pamelafox/b98e03caca7d1ec14394a90ec1512cff */
+-- I perfomed the following analysis on the 'NASA Astronauts, 1959-Present' dataset from Kaggle.
+-- Link to Dataset in Kaggle: https://www.kaggle.com/nasa/astronaut-yearbook
+-- Table source: https://gist.github.com/pamelafox/b98e03caca7d1ec14394a90ec1512cff */
 
--- Let's look at the gender distribution
+#1 - Let's look at the gender distribution
 SELECT gender, COUNT(*) FROM astronauts
 GROUP BY gender;
 
--- In terms of education and military rank, what does it 'take' to be an Astronaut?
+#2 - In terms of education and military rank, what does it 'take' to be an Astronaut?
 SELECT 
 CASE
     WHEN military_rank IS NOT NULL AND (undergraduate_major IS NOT NULL OR graduate_major IS NOT NULL) then 'Ranked Officer, Degree Obtained'
@@ -19,16 +19,16 @@ FROM astronauts
 GROUP BY rank_and_education
 ORDER BY 2 desc;
 
--- I want to know which Astronauts were part of the series of Apollo missions...
+#3 - I want to know which Astronauts were part of the series of Apollo missions...
 SELECT name, missions FROM astronauts
 WHERE missions LIKE '%Apollo%';
 
--- Which institutions can claim to be the Alma Mater for at least 4 Astronauts?
+#4 - Which institutions can claim to be the Alma Mater for at least 4 Astronauts?
 SELECT alma_mater, COUNT(*) FROM astronauts
 GROUP BY alma_mater
 HAVING COUNT(*) >= 4
 ORDER BY 2 DESC
 
--- What was the average number of hours spent doing spacewalks for those Astronauts who managed to do it at least once?
+#5 -  What was the average number of hours spent doing spacewalks for those Astronauts who managed to do it at least once?
 SELECT AVG(space_walks_hr) FROM astronauts
 WHERE space_walks > 0;
