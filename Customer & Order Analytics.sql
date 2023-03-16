6becc0eab3c93ed43f52d9acf2a381a38b6a173e
Miguel Ojeda <126533982+Miguelol3@users.noreply.github.com>
2023-03-15 22:33:21 -0400

    Update Customer & Order Analytics.sql

--- Customer & Order Analytics.sql
+++ Customer & Order Analytics.sql
@@ -6,11 +6,13 @@ SELECT COUNT(orderID) FROM bit_db.JanSales
 WHERE LENGTH(orderID) = 6 
 AND orderID <> 'Order ID';
 
+
 #2 - How many of those orders were for an iPhone?
 SELECT COUNT(*) FROM bit_db.JanSales
 WHERE product = 'iPhone'
 AND LENGTH(orderID) = 6
 AND orderID <> 'Order ID';
+
 
 #3 - Select the customer account numbers for all the orders that were placed in February.
 SELECT distinct (c.acctnum)
@@ -19,10 +21,12 @@ INNER JOIN bit_db.FebSales AS fs ON c.order_id = fs.orderID
 WHERE LENGTH(fs.orderid) = 6
 AND fs.orderid <> 'Order ID';
 
+
 #4 - Which product was the cheapest one sold in January, and what was the price?
 SELECT distinct (product), price
 FROM bit_db.JanSales
 WHERE price IN (SELECT MIN(price) FROM bit_db.JanSales WHERE price <> 'Price Each' AND price <> '');
+
 
 #5 - What is the total revenue for each product sold in January?
 SELECT product, ROUND((SUM(quantity)*price), 2) AS 'Product Revenue'
@@ -30,6 +34,7 @@ FROM bit_db.JanSales
 WHERE price <> 'Price Each' AND price <> '' AND product <> 'Product'
 GROUP BY 1
 ORDER BY 2 DESC;
+
 
 #6 - Which products were sold in February at 548 Lincoln St, Seattle, WA 98101, how many of each were sold, and what was the total revenue?
 SELECT 
@@ -39,6 +44,7 @@ SELECT
 FROM bit_db.FebSales 
 WHERE location = '548 Lincoln St, Seattle, WA 98101'
 GROUP BY 1;
+
 
 #7 - How many customers ordered more than 2 products at a time in February, and what was the average amount spent for those customers?
 SELECT
@@ -50,11 +56,13 @@ WHERE fs.quantity > 2
 AND LENGTH(fs.orderID) = 6
 AND fs.orderID <> 'Order ID';
 
+
 #8 - List all the products sold in Los Angeles in February, and include how many of each were sold.
 SELECT product, SUM(quantity) FROM bit_db.FebSales
 WHERE location LIKE '%Los Angeles%'
 GROUP BY 1
 ORDER BY 2 DESC;
+
 
 #9 - Which locations in New York received at least 3 orders in January, and how many orders did they each receive?
 SELECT location, COUNT(orderid)
@@ -65,6 +73,7 @@ AND orderID <> 'Order ID'
 GROUP BY 1
 HAVING COUNT(orderid) >= 3
 ORDER BY 2 DESC;
+
 
 #10 - How many of each type of headphone were sold in February?
 SELECT product, SUM(quantity) 
@@ -87,7 +96,6 @@ FROM bit_db.FebSales AS fs
 LEFT JOIN bit_db.customers AS c ON fs.orderID = c.order_id
 WHERE length(fs.orderid) = 6
 AND fs.orderID <> 'Order ID';
-
 
 
 #13 - Which product brought in the most revenue in January and how much revenue did it bring in total?
