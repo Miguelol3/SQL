1d5562ae4ab51e4fea52f2562e2218fad126ecbb
Miguel Ojeda <126533982+Miguelol3@users.noreply.github.com>
2023-03-22 21:32:50 -0400

    Create Spotify Analytics.sql

---
+++ Spotify Analytics.sql
@@ -0,0 +1,57 @@
+-- For the following exploration, I downloaded the 'Spotify Top 50 Songs in 2021' dataset from Kaggle.
+-- Source: https://www.kaggle.com/datasets/equinxx/spotify-top-50-songs-in-2021
+-- I started by creating a table and then inserted the data into it via the import tool in SQLiteStudio.
+
+-- Table Creation:
+CREATE TABLE Master_DB.Spotifydata (
+  id INTEGER PRIMARY KEY,
+  artist_name VARCHAR NOT NULL,
+  track_name VARCHAR NOT NULL,
+  track_id VARCHAR NOT NULL,
+  popularity INTEGER NOT NULL,
+  danceability DECIMAL(4,3) NOT NULL,
+  energy DECIMAL(4,3) NOT NULL,
+  song_key INTEGER NOT NULL,
+  loudness DECIMAL(5,3) NOT NULL,
+  song_mode INTEGER NOT NULL,
+  speechiness DECIMAL(5,4) NOT NULL,
+  acousticness DECIMAL(6,5) NOT NULL,
+  instrumentalness DECIMAL(8,7) NOT NULL,
+  liveness DECIMAL(5,4) NOT NULL,
+  valence DECIMAL(4,3) NOT NULL,
+  tempo DECIMAL(6,3) NOT NULL,
+  duration_ms INTEGER NOT NULL,
+  time_signature INTEGER NOT NULL);
+  
+
+#1. Which 3 artists were the most 'successful' in 2021?
+SELECT artist_name, COUNT(*) FROM spotifydata
+GROUP BY artist_name
+ORDER BY 2 desc
+LIMIT 3;
+
+
+#2. I feel like throwing a small gathering, which songs performed better than average in terms of danceability and energy?
+SELECT
+artist_name
+,track_name
+,popularity
+,danceability
+,energy
+FROM spotifydata 
+WHERE danceability > (SELECT AVG(danceability) FROM spotifydata)
+AND energy > (SELECT AVG(energy) FROM spotifydata)
+ORDER BY 3 DESC;
+
+#3. Okay, I made the list - how many hours of music did I end up with?
+WITH main AS (
+SELECT * FROM spotifydata 
+WHERE danceability > (SELECT AVG(danceability) FROM spotifydata)
+AND energy > (SELECT AVG(energy) FROM spotifydata)
+)
+
+SELECT 
+SUM (duration_ms) / 60000 AS minutes
+FROM main
+
+/* It seems like we can only have a pregame, not a full on party :( */
